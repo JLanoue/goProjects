e195eb0dc99db67252aa5393a8752391448ef423
Jeren Lanoue <jlanoue@Jerens-MacBook-Air.local>
2018-07-09 21:46:22 -0400

    updated working number of hordes input

--- main/ethnosCardPicker_02/main.go
+++ main/ethnosCardPicker_02/main.go
@@ -7,19 +7,28 @@ import (
 )
 
 func main() {
-	vals := []string{"Centaurs", "Dwarves", "Elves", "Giants", "Halflings", "Merfolk", "Minotaurs",
+
+	var x int
+	fmt.Println("How many classes do you need?")
+
+	_, err := fmt.Scanf("%d", &x)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	hordes := []string{"Centaurs", "Dwarves", "Elves", "Giants", "Halflings", "Merfolk", "Minotaurs",
 		"Orcs", "Skeletons", "Trolls", "Wingfolk", "Wizard"}
 	//var classesPicked = make([]string, 5)
 
-	r := rand.New(rand.NewSource(time.Now().Unix()))
-	for _, i := range r.Perm(len(vals)) {
-		val := vals[i]
-		fmt.Println(val)
-	}
-}
+	var S []string
 
-/*
-func main() {
-	rand.Seed(time.Now().UTC.UnixNano())
+	r := rand.New(rand.NewSource(time.Now().Unix()))
+	for _, i := range r.Perm(len(hordes)) {
+		S = append(S, hordes[i])
+		//fmt.Print(s[:])
+		//hordeSelection := strings.Fields(val)
+		//fmt.Println(hordeSelection[0:6])
+	}
+	fmt.Println(S[:(x)])
 }
-*/
